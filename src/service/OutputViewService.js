822840a798efe7b884de41e7836f90486d961263
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-12-01 18:25:37 +0900

    feat: 경주 게임의 한번의 결과를 출력하는 기능구현
    -OutputView 에서 차 1대의 상태값 출력
    -OutputViewService 에서 1번의 게임 결과를 출력

--- src/service/OutputViewService.js
+++ src/service/OutputViewService.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+import OutputView from "../view/OutputView.js";
+
 const OutputViewService = {
-	fn: () => {},
+	printOnceRacingResult: (carList) => {
+		Console.print("");
+		carList.forEach((car) => {
+			const [name, position] = car.getNameAndPosition();
+			OutputView.printCarAndPosition(name, position);
+		});
+	},
 };
 
 export default OutputViewService;
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,13 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
 import { OUTPUT_VIEW } from "../utils/Constants.js";
-import OutputViewService from "../service/OutputViewService.js";
 
 const OutputView = {
-	printGameStart() {
-		Console.print(OUTPUT_VIEW.startGame); //바꾸고
-	},
-	printMenu() {
-		Console.print(OUTPUT_VIEW);
+	printCarAndPosition(name, position) {
+		Console.print(`${name} : ${"-".repeat(position)}`); //바꾸고
 	},
 	// ...
 };

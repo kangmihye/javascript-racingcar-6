ed19994a55d314e934215aaaa768d623d547b657
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-12-01 17:08:53 +0900

    feat: 차이름 유효성 검사
    - 5자 이하면 에러

--- src/service/validateInput.js
+++ src/service/validateInput.js
@@ -1,8 +1,6 @@
 import { ERROR } from "../utils/Constants.js";
-import { isInvalidNumber, isDuplicated } from "../utils/conditions.js";
+import { isInvalidNameSize } from "../utils/conditions.js";
 
-export const validatePrice = (input) => {
-	//함수명 바꿔라~~
-	if (isInvalidNumber(input)) throw new Error(ERROR.isInvalidNum);
-	if (isDuplicated(input)) throw new Error(ERROR.isDuplicated);
+export const validateCarName = (input) => {
+	if (isInvalidNameSize(input)) throw new Error(ERROR.isInvalidNameSize);
 };
--- src/utils/conditions.js
+++ src/utils/conditions.js
@@ -1,5 +1,12 @@
 import {} from "../utils/Constants.js";
 
+export const isInvalidNameSize = (input) => {
+	//true면 5자 이상
+	const carNamesArr = input.split(",");
+	return carNamesArr.some((name) => name.length > 5);
+};
+
+///
 export const isInvalidNumber = (input) => {
 	// true면 숫자아님
 	input = Number(input.replaceAll(" ", ""));
